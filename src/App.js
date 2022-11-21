db839a0e0c5dcb076225595891dce1a1050a9838
whjin <1414848321@qq.com>
2022-11-21 21:24:43 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PostList from "./PostList";
+
+function App() {
+  return (
+    <div>
+      <PostList></PostList>
+    </div>
+  );
+}
+
+export default App;
