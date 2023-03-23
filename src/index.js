999334fc8f75629cde2c1bcb132f526fdaaf03b5
whjin <1414848321@qq.com>
2023-03-23 15:45:02 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </Provider>
 );
 
--- src/views/home/404.js
+++
@@ -1,8 +0,0 @@
-function UndefinedView () {
-    return <div>
-        <h1>404视图</h1>
-        <p>404视图内容</p>
-    </div>;
-}
-
-export default UndefinedView;
