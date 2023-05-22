88f28594146a72564b97d91cd078e900011a1200
whjin <1414848321@qq.com>
2023-05-22 19:09:25 +0800

    add

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -5,17 +5,21 @@ import ContactList from "../../componnet/contact/ContactList";
 import Parent from "../../componnet/context/Parent";
 import Counter from "../../componnet/counter";
 import TodoPage from "../../componnet/todos";
+import Product from "../../componnet/product";
 
 class Home extends React.Component {
-    render () {
-        return <div className="app-container">
-            <Parent />
-            <PostList />
-            <ContactList />
-            <Counter />
-            <TodoPage />
-        </div>;
-    }
+  render() {
+    return (
+      <div className="app-container">
+        <Parent />
+        <PostList />
+        <ContactList />
+        <Counter />
+        <TodoPage />
+        <Product />
+      </div>
+    );
+  }
 }
 
 export default Home;
