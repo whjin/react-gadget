8bac001654f09c5ba4cb7fa8509a6b75055b3803
whjin <1414848321@qq.com>
2023-06-02 15:57:23 +0800

    add

--- src/componnet/contact/ContactList.js
+++ src/componnet/contact/ContactList.js
@@ -1,42 +1,17 @@
-import React from "react";
-import ContactItem from "./ContactItem";
-import contactList from "../../static/mock/contactList.json";
-import TravelPlan from '../travelPlan/TravelPlan';
-import MailClient from "../mailClient/MailClient";
-import TaskApp from "../taskApp/TaskApp";
-
-class ContactList extends React.Component {
-  state = {
-    openName: "",
-  };
-
-  toggleOpen = (name) => {
-    this.setState({ openName: name });
-  };
-
-  render () {
-    const { openName } = this.state;
-    return (
-      <div className="common-container">
-        <section className="contact-list">
-          {Object.keys(contactList).map((item) => {
-            return (
-              <ContactItem
-                key={contactList[item].id}
-                contact={contactList[item]}
-                name={item}
-                openName={openName}
-                toggleOpen={this.toggleOpen}
-              />
-            );
-          })}
-        </section>
-        <TaskApp />
-        <TravelPlan />
-        <MailClient />
-      </div>
-    );
-  }
-}
-
-export default ContactList;
+export default function contactList ({ contacts, selectedId, onSelect }) {
+  return (
+    <>
+      <ul>
+        {
+          contacts.map(contact => (
+            <li key={contact.id}>
+              <button onClick={() => onSelect(contact.id)}>
+                {contact.id === selectedId ? <b>{contact.name}</b> : contact.name}
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+} 
